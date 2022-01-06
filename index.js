b676b9ec52b315c8a0bdd55bd7efdb6bf4d1ebca
Robin Lans <robinlans@Mac-mini-som-tillhor-Robin.local>
2022-01-06 02:55:42 +0100

    add db

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
 const { Pool } = require("pg");
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL || process.env.DB_URI,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/select", (req, res) => {
+  db.query("SELECT * FROM test;", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    const data = result.rows;
+    console.log(data);
+    res.json(data);
+  });
+});
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("Server has started");
