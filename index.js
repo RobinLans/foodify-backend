090d3c376c911d85764420858df5b98b2fca80db
Robin Lans <robinlans@Mac-mini-som-tillhor-Robin.local>
2022-01-06 20:39:14 +0100

    add endpints to server

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/select", (req, res) => {
-  db.query("SELECT * FROM test;", (err, result) => {
+  db.query("SELECT * FROM users;", (err, result) => {
     if (err) {
       console.log(err);
     }
@@ -23,6 +23,42 @@ app.get("/select", (req, res) => {
     console.log(data);
     res.json(data);
   });
+});
+
+app.post("/sign-in", (req, res) => {
+  let success = false;
+  db.query(
+    `SELECT name, password FROM users WHERE name = '${req.body.username}' AND password = '${req.body.password}';`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      const data = result.rows;
+      if (data.length > 0) success = true;
+      res.json(success);
+    }
+  );
+});
+
+app.post("/register", (req, res) => {
+  let registration = {
+    success: false,
+  };
+
+  db.query(
+    `INSERT INTO public.users(name, password, favorites) VALUES ('${req.body.username}', '${req.body.password}', '[]');`,
+    (err, result) => {
+      if (err) {
+        if (err.code == 23505) {
+          registration.message = "username already exists";
+        }
+      } else if (result) {
+        registration.success = true;
+        registration.message = "user created successfully";
+      }
+      res.json(registration);
+    }
+  );
 });
 
 app.listen(process.env.PORT || 4000, () => {
